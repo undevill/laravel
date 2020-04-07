5e016370f02358492e80dc37c8540cd0fcc9fc3f
undevill <50099149+undevill@users.noreply.github.com>
2020-04-07 07:45:39 +0300

    css js

--- public/index.php
+++ public/index.php
@@ -35,9 +35,9 @@ require __DIR__.'/../vendor/autoload.php';
 |
 */
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once __DIR__.'/../bootstrap/app.php'
+/*;
 
-/*
 |--------------------------------------------------------------------------
 | Run The Application
 |--------------------------------------------------------------------------
